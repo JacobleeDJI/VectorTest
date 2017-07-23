9fdd7e95a42ac055f49c7b96d55572cfb0d76506
Jacoblee <jacobleeloveru@gmail.com>
2017-07-23 10:50:46 +0800

    Initial Commit

---
+++ vector_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vector_1
+//
+//  Created by Jacoblee on 2017/7/23.
+//  Copyright © 2017年 Jacoblee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
